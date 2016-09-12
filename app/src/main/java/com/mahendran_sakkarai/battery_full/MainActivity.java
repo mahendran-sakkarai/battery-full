417de69101230022beb56ec8e3d44e95fffa70d7
Mahendran Sakkarai <mahendranmails@gmail.com>
2016-09-13 01:02:08 +0530

    Added Main Activity

---
+++ app/src/main/java/com/mahendran_sakkarai/battery_full/MainActivity.java
@@ -0,0 +1,31 @@
+package com.mahendran_sakkarai.battery_full;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSettings();
+            }
+        });
+    }
+
+    private void goToSettings() {
+        Intent settingIntent = new Intent(this, SettingsActivity.class);
+        startActivity(settingIntent);
+    }
+}
